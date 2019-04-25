3601cbf2279eeccc84ff2d796e39142a4b8c673c
marudor <marudor@marudor.de>
2019-04-25 18:06:33 +0200

    dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-  plugins: [],
-  overrides: [
-    {
-      test: ['./src/**/*.ts'],
-      presets: [
-        '@babel/preset-typescript',
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: '10',
-            },
-            loose: false,
-            useBuiltIns: 'entry',
-            modules: 'commonjs',
-            corejs: 3,
-          },
-        ],
-        'babel-preset-joblift',
-      ],
-    },
+  comments: false,
+  presets: [
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '10',
+        },
+        loose: false,
+        useBuiltIns: 'entry',
+        modules: 'commonjs',
+        corejs: 3,
+      },
+    ],
+    'babel-preset-joblift',
   ],
+  plugins: [],
 };
