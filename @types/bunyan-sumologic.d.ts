c7438463823e01adc662dff2d288369755066786
marudor <marudor@marudor.de>
2019-06-11 17:03:14 +0200

    Optionally log with sumologic

---
+++ @types/bunyan-sumologic.d.ts
@@ -0,0 +1,3 @@
+declare module 'bunyan-sumologic' {
+  export default any;
+}
--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,7 @@ import bunyan, { INFO } from 'bunyan';
 import BunyanFormat from 'bunyan-format';
 import bunyanLoggly from 'bunyan-loggly';
 import bunyanMiddleware from 'koa-bunyan-logger';
+import bunyanSumoLogic from 'bunyan-sumologic';
 
 const accessConfig = {
   level: INFO,
@@ -21,16 +22,31 @@ const logglyConfig = {
   token: process.env.LOGGLY_TOKEN,
   subdomain: process.env.LOGGLY_SUBDOMAIN,
 };
+const sumoLogicConfig = {
+  collector: process.env.SUMO_COLLECTOR,
+  endpoint: 'https://endpoint1.collection.eu.sumologic.com/receiver/v1/http/',
+};
 
-if (process.env.NODE_ENV === 'production' && logglyConfig.token && logglyConfig.subdomain) {
-  // eslint-disable-next-line no-console
-  console.log('Using loggly to log');
-  accessConfig.streams.push({
-    // @ts-ignore
-    stream: new bunyanLoggly(logglyConfig),
-    // @ts-ignore
-    type: 'raw',
-  });
+if (process.env.NODE_ENV === 'production') {
+  if (logglyConfig.token && logglyConfig.subdomain) {
+    // eslint-disable-next-line no-console
+    console.log('Using loggly to log');
+    accessConfig.streams.push({
+      // @ts-ignore
+      stream: new bunyanLoggly(logglyConfig),
+      // @ts-ignore
+      type: 'raw',
+    });
+  } else if (sumoLogicConfig.collector) {
+    // eslint-disable-next-line no-console
+    console.log('Using sumo to log');
+    accessConfig.streams.push({
+      // @ts-ignore
+      stream: new bunyanSumoLogic(sumoLogicConfig),
+      // @ts-ignore
+      type: 'raw',
+    });
+  }
 }
 
 const accessLogger = bunyan.createLogger(accessConfig);
