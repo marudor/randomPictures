11f5b4b93c4ce938c8c9bc29701ba59badd72146
marudor <marudor@marudor.de>
2022-09-29 16:53:49 +0200

    fix: remove loggly

--- src/logger/logWriteThread.js
+++ src/logger/logWriteThread.js
@@ -7,31 +7,14 @@ const prettyLog = pinoPretty.prettyFactory({
   colorize: true,
   translateTime: true,
 });
-const streams = [
-  (msg) => {
-    process.stdout.write(prettyLog(JSON.stringify(msg)));
-  },
-];
+const streams = [];
 
-const logglyToken = process.env.LOGGLY_TOKEN;
-
-if (process.env.NODE_ENV === 'production' && logglyToken) {
-  // eslint-disable-next-line no-console
-  console.log('Using loggly to log');
-  streams.push((msg) => {
-    // 30 is info, we log info and above to loggly
-    if (msg.level >= 30) {
-      axios.post(`https://logs-01.loggly.com/inputs/${logglyToken}`, JSON.stringify(msg), {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-    }
-  });
+if (process.env.PRETTY_LOG) {
+  streams.push((msg) => process.stdout.write(prettyLog(msg)));
+} else {
+  streams.push((msg) => console.log(msg));
 }
 
 parentPort.on('message', (msg) => {
-  const parsedMsg = JSON.parse(msg);
-
-  streams.forEach((s) => s(parsedMsg));
+  for (const s of streams) s(msg);
 });
