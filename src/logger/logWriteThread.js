26161332b622c7dc7d8e6a24bd2dd6cc115b17f5
marudor <marudor@marudor.de>
2022-10-01 17:54:34 +0200

    fix pino-pretty only loaded if specified

--- src/logger/logWriteThread.js
+++ src/logger/logWriteThread.js
@@ -1,15 +1,16 @@
 const { parentPort } = require('node:worker_threads');
 const axios = require('axios');
 const { Timber } = require('@timberio/node');
-const pinoPretty = require('pino-pretty');
 
-const prettyLog = pinoPretty.prettyFactory({
-  colorize: true,
-  translateTime: true,
-});
 const streams = [];
 
 if (process.env.PRETTY_LOG) {
+  const pinoPretty = require('pino-pretty');
+
+  const prettyLog = pinoPretty.prettyFactory({
+    colorize: true,
+    translateTime: true,
+  });
   streams.push((msg) => process.stdout.write(prettyLog(msg)));
 } else {
   streams.push((msg) => console.log(msg));
